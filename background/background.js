77beded53bf28c1466621041629342afe573a918
Anthony Suárez <neolight1010@gmail.com>
2021-07-29 17:36:19 -0500

    Allow blocking sites using storage.sync

--- background/background.js
+++ background/background.js
@@ -6,10 +6,28 @@ function onBeforeRequest(_details) {
   };
 }
 
-browser.webRequest.onBeforeRequest.addListener(
-  onBeforeRequest,
-  {
-    urls: ["*://*.facebook.com/*"],
-  },
-  ["blocking"]
-);
+function allUrlsInDomain(domain) {
+  return `*://${domain}/*`;
+}
+
+function getBlockedWebsites() {
+  const storage = browser.storage.sync;
+
+  const defaultBlockedSites = [
+    allUrlsInDomain("www.facebook.com"),
+    allUrlsInDomain("www.youtube.com"),
+    allUrlsInDomain("www.twitch.tv"),
+  ];
+
+  return storage.get({ blockedSites: defaultBlockedSites });
+}
+
+getBlockedWebsites().then((data) => {
+  browser.webRequest.onBeforeRequest.addListener(
+    onBeforeRequest,
+    {
+      urls: data.blockedSites,
+    },
+    ["blocking"]
+  );
+});
