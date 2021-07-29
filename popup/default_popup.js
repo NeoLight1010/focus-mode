5404ac5aa4c097d50564eab2cc7c5a5535291cbe
Anthony Suárez <neolight1010@gmail.com>
2021-07-29 13:54:29 -0500

    Add form validation and storage.

--- popup/default_popup.js
+++ popup/default_popup.js
@@ -1,9 +1,51 @@
-function onSubmit(form) {
-  console.log(form);
+function onSubmit(e) {
+  const storage = browser.storage.sync;
+  const data = new FormData(e.target);
 
-  const data = new FormData(form.target);
-  console.log(...data);
+  const startTimeString = `${data.get("start-hour")}:${data.get(
+    "start-minute"
+  )}`;
+  const endTimeString = `${data.get("end-hour")}:${data.get("end-minute")}`;
+
+  const startTime = new Date("1970-01-01T" + startTimeString + "Z");
+  const endTime = new Date("1970-01-01T" + endTimeString + "Z");
+
+  // Validation
+  let isInvalid = false;
+
+  if (startTime.toString() === "Invalid Date") {
+    document.getElementById("start-hour").classList.add("uk-form-danger");
+    document.getElementById("start-minute").classList.add("uk-form-danger");
+
+    console.log("Start time is invalid!");
+    isInvalid = true;
+  }
+
+  if (endTime.toString() === "Invalid Date") {
+    document.getElementById("end-hour").classList.add("uk-form-danger");
+    document.getElementById("end-minute").classList.add("uk-form-danger");
+
+    console.log("End time is invalid!");
+    isInvalid = true;
+  }
+
+  if (isInvalid) {
+    e.preventDefault();
+    return false;
+  }
+
+  storage.set({
+    startTime,
+    endTime,
+  });
+
+  return true;
 }
 
 const timeForm = document.getElementById("time-form");
 timeForm.addEventListener("submit", onSubmit);
+
+console.log(
+  browser.storage.sync.get("startTime"),
+  browser.storage.sync.get("endTime")
+);
